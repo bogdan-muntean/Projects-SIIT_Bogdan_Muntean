9b02deef1cd7a1fd96d04fe45866db3341d6affc
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-02-18 12:26:20 +0200

    first edit

---
+++ week4_homework/homework.js
@@ -0,0 +1,46 @@
+var array = [ 1, 2, 3, 4, 56, 232, 45, 12, 4, 6, 8, 8,8, 1, 1]
+
+// 1. display in the console the numbers from 1 to 20
+
+// for(var i = 0; i < array.length; i++){
+//     if(array[i] >= 1 && array[i] <= 20){
+//         console.log(array[i]);
+//     }
+// }
+
+// // 2. display in the console the odd numbers from 1 to 20
+
+// for(var i = 0; i < array.length; i++){
+//     if((array[i] % 2) != 0){
+//         console.log(array[i]);
+//     }
+// }
+
+// 3. compute the sum of the elements of an array and display it in the console
+// var sum = 0;
+// for(var i = 0; i < array.length; i++){
+//     sum += array[i];
+// }
+// console.log(sum);
+
+// 4. compute the maximum of the elements of an array and display it in the console
+// var maxim = 0;
+// for(var i = 0; i < array.length; i++){
+//     if(array[i] > maxim){
+//         maxim = array[i];
+//     }
+// }
+// console.log(maxim);
+
+// 5. compute how many times a certain element appears in an array - tips: use object, var element = 2
+Object
+
+for(var i = 0; i < array.length; i++){
+
+}
+
+// 6. Using nested control structures (doua for + if) for generate the following pattern
+//   0 1 0 1
+//   1 0 1 0
+//   0 1 0 1
+//   1 0 1 0
---
+++ week4_homework/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+for(var i = 0; i < array.length; i++){
+//     if(array[i] >= 1 && array[i] <= 20){
+//         console.log(array[i]);
+//     }
+// }
