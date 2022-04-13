2cf8029784da6c8f0cfb0910d112dfc6c0fb315d
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-04-13 22:42:49 +0300

    completed ex 6

--- week11_homework/app.js
+++ week11_homework/app.js
@@ -20,19 +20,19 @@ const player = new Player(gameContainer);
 //-cream eveniment de la tastatura pentru a misca player-ul
 document.addEventListener("keydown", (keydownEvent) => {
     console.log(keydownEvent)
-    if(keydownEvent.code === "ArrowRight"){
+    if (keydownEvent.code === "ArrowRight") {
         player.moveRight();
     }
-    if(keydownEvent.code === "ArrowLeft"){
+    if (keydownEvent.code === "ArrowLeft") {
         player.moveLeft();
     }
-    if(keydownEvent.code === "ArrowUp"){
+    if (keydownEvent.code === "ArrowUp") {
         player.moveUp();
     }
-    if(keydownEvent.code === "ArrowDown"){
+    if (keydownEvent.code === "ArrowDown") {
         player.moveDown();
     }
-        
+
 })
 
 //cream o lista unde adaugam monstrii, din lista asta vom luam pe rand monstrii pentru a le creea un setInterval
@@ -40,58 +40,58 @@ document.addEventListener("keydown", (keydownEvent) => {
 const listMonster = []
 
 //-cream monstrii + adaugam monstrii pe pozitii random in container + adaugam in listMonster[]
-for(let i = 0; i < 30; i++){
+for (let i = 0; i < 30; i++) {
     const monster = new Monster(gameContainer);
     listMonster.push(monster);
 }
 
-//sa se miste random pe harta
-setInterval(() => {
-    for(let i = 0; i < listMonster.length; i++){
-        const monsterElement = listMonster[i];
-        moveMonster(monsterElement);
-    } 
-    console.log(gameOver);
-}, 100);
-
-
 //6. sa verific daca player-ul a atins vreun monstru
 var gameOver = false;
 
-//6. Functia prin care daca player intalneste un monstru sa fie game over
-() => {
-    for(let i = 0; i < listMonster.length; i++){
-    const monsterElement = listMonster[i];
+//sa se miste random pe harta
+setInterval(() => {
+    for (let i = 0; i < listMonster.length; i++) {
+        const monsterElement = listMonster[i];
+        moveMonster(monsterElement);
+    }
+    console.log(gameOver);
 
-    const positionPlayerTop = parseInt(player.style.top); 
-    const positionPlayerLeft = parseInt(player.style.left); 
-    const positionMonsterTop = parseInt(monsterElement.style.top);
-    const positionMonsterLeft = parseInt(monsterElement.style.left);
+    //6. Functia prin care daca player intalneste un monstru sa fie game over
+    for (let i = 0; i < listMonster.length; i++) {
+        const monsterElement = listMonster[i].element;
 
-        if(positionMonsterTop >= positionPlayerTop && positionMonsterTop <= (positionPlayerTop + 30) &&
-            positionMonsterLeft >= positionPlayerLeft && positionMonsterLeft <= (positionPlayerLeft + 30)){
+        const positionPlayerTop = parseInt(player.element.style.top);
+        const positionPlayerLeft = parseInt(player.element.style.left);
+        const positionMonsterTop = parseInt(monsterElement.style.top);
+        const positionMonsterLeft = parseInt(monsterElement.style.left);
+
+        if (positionMonsterTop >= positionPlayerTop && positionMonsterTop <= (positionPlayerTop + 30) &&
+            positionMonsterLeft >= positionPlayerLeft && positionMonsterLeft <= (positionPlayerLeft + 30)) {
             console.log("GAME OVER");
+            alert("GAME OVER")
             gameOver = true;
         }
-
     }
-}
+}, 100);
 
-function moveMonster(myMonster){
+
+
+
+function moveMonster(myMonster) {
     const movementList = ["ArrowRight", "ArrowLeft", "ArrowUp", "ArrowDown"];
     const randomNumber = Math.floor(Math.random() * 4);
     const randomMovement = movementList[randomNumber]
 
-    if(randomMovement === "ArrowRight"){
+    if (randomMovement === "ArrowRight") {
         myMonster.moveRight();
     }
-    if(randomMovement === "ArrowLeft"){
+    if (randomMovement === "ArrowLeft") {
         myMonster.moveLeft();
     }
-    if(randomMovement === "ArrowUp"){
+    if (randomMovement === "ArrowUp") {
         myMonster.moveUp();
     }
-    if(randomMovement === "ArrowDown"){
+    if (randomMovement === "ArrowDown") {
         myMonster.moveDown();
     }
 }
