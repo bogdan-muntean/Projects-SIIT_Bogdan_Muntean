b7ada7d35c7a6362b34499b3cc857285e1987f58
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-05-20 12:19:52 +0300

    edit

---
+++ week10_optional-hangman/app.js
@@ -0,0 +1,18 @@
+// (Optional) Homework HangMan +4p
+// Tasks:
+
+//     O lista de cuvinte din care extragem un cuvant random
+//     Un mod de a introduce litere
+//     Sa verificam daca literea introdusa face parte din cuvant
+//     Sa afisam cate o liniuta pentru fiecare litera din cuvantul ales
+//     Daca literea apare in cuvant atunci punem in locul liniutei cu litera
+//     Daca gasim o literea, le afisam pe toate de acelasi tip
+//     Un number de vieti, daca ajunge la zero este game over
+
+// Folosirea stilului OOP, indicati:
+
+//     Facem o clasa HangMan iar pe contextul clasei tinem lista de cuvinte
+//     Facem o metoda pentru afisarea de liniute. Daca avem cuvantul "javascript", pe ecran afisam _ _ _ _ _ _ _ _ _ _ ( 10 linuite)
+//     O metoda unde obtinem ce a introdus utilizatorul
+
+//     Incercati sa spargi codul in cat mai multe functii/metode
